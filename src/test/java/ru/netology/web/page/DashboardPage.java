87f08bc85e994e1333d0a6486ab3a32cf28b3a44
chrisemenova <chrisemenova88@gmail.com>
2022-11-24 20:32:00 +0300

    initial commit

---
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -0,0 +1,43 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+
+public class DashboardPage {
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+    @FindBy(css = "[data-test-id=dashboard]")
+    private SelenideElement heading;
+//  private SelenideElement heading = $("[data-test-id=dashboard]");
+    @FindBy(css= ".list__item div")
+    private ElementsCollection cards;
+//  private ElementsCollection cards = $$(".list__item div");
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+    }
+
+    public int getCardBalance(DataHelper.CardInfo cardInfo) {
+    var text = cards.findBy(Condition.text(cardInfo.getCardNumber().substring(12, 16))).getText();
+        return extractBalance(text);
+    }
+
+    private int extractBalance(String text) {
+        var start = text.indexOf(balanceStart);
+        var finish = text.indexOf(balanceFinish);
+        var value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+
+    public TransferPage selectCardToTransfer(DataHelper.CardInfo cardInfo) {
+        cards.findBy(attribute("data-test-id", cardInfo.getTestId())).$("button").click();
+        return new TransferPage();
+    }
+
+}
---
+++ src/test/java/ru/netology/web/page/VerificationPage.java
@@ -0,0 +1,26 @@
+package ru.netology.web.page;
+
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.*;
+
+public class VerificationPage {
+
+    @FindBy(css = "[data-test-id=code] input")
+    private SelenideElement codeField;
+    @FindBy(css = "[data-test-id=action-verify]")
+    private SelenideElement verifyButton;
+
+    public VerificationPage(){
+        codeField.shouldBe(visible);
+    }
+
+    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
+        codeField.setValue(verificationCode.getCode());
+        verifyButton.click();
+        return new DashboardPage();
+    }
+}
